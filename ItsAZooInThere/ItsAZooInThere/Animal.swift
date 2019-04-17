21a475c56984a157b025dc129cf6ea1e054659d0
Shengsheng Xu <ssxu1225@cs.uchicago.edu>
2019-04-17 03:50:06 -0500

    add zoo

---
+++ ItsAZooInThere/ItsAZooInThere/Animal.swift
@@ -0,0 +1,9 @@
+//
+//  Animal.swift
+//  ItsAZooInThere
+//
+//  Created by Shengsheng Xu on 4/16/19.
+//  Copyright © 2019 Shengsheng Xu. All rights reserved.
+//
+
+import Foundation
---
+++ ItsAZooInThere/ItsAZooInThere/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ItsAZooInThere
+//
+//  Created by Shengsheng Xu on 4/16/19.
+//  Copyright © 2019 Shengsheng Xu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
